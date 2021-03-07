629f1cba67c08e396fbb0a6c6445a23846b6a3ba
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-03-07 10:53:27 +0200

    added code:

--- snowman.cpp
+++ snowman.cpp
@@ -23,99 +23,49 @@ const array<std::string,4> bases = {" : ", "\" \"", "___","   "};
 const int snowman1=11114411;
 const int snowman2=33232124;
 const int INPUT_SIZE_CHECK=10000000;
-class Snowman{
-    public:
-        std::string hat;
-        std::string left;
-        std::string left_eye;
-        std::string right_eye;
-        std::string left_arm_top;
-        std::string left_arm;
-        std::string right_arm_top;
-        std::string right_arm ;
-        std::string torso;
-        std::string base;
-        void Snowman(int type){
-            base = bases[type%10];
-        }
-        std::string toString(){
-            return " "+hat+"\n"+left_arm_top+"("+left_eye+nose+right_eye+")"+right_arm_top+"\n"+left_arm
-            +"("+torso+")"+right_arm+"\n "+" ("+base+")";
-        }
-};
-std::array<int,8> parse_input(int type){
-    array<int,8> build_snowman;
-    for (int i = 0; i < 8; ++i) {
-        build_snowman[i] = type/(INPUT_SIZE_CHECK/pow(10,i));
-        type = type%(INPUT_SIZE_CHECK/pow(10,i));
-    }
-}
+const int HAT = 0, NOSE =1 , LEFT_EYE = 2, RIGHT_EYE = 3, LEFT_ARM=4,RIGHT_ARM=5,TORSO=6,BASE=7;
 
-std::string build_face(array<int,8> type){
-    std::string face = "("+left_eyes[type[2]-1]+noses[types[1]-1]-+right_eyes[types[3]-1]+")";
+std::string build_face(array<int,8> types){
+    std::string face = left_arm_tops[types[LEFT_ARM]-1]+"("+left_eyes[types[LEFT_EYE]-1]+noses[types[NOSE]-1]+right_eyes[types[RIGHT_EYE]-1]+")"+right_arm_tops[types[RIGHT_ARM]];
     return face;
 }
-std::string build_body(int type){
-    std::string body ="("+torsos[type]+")";
+std::string build_body(array<int,8> types){
+    std::string body =left_arms[types[LEFT_ARM]-1]+"("+torsos[types[TORSO]-1]+")"+right_arms[types[RIGHT_ARM]-1];
     return body;
 }
 std::string build_base(int type){
-    std::string lower_part = "("+bases[type]+ ")";
+    std::string lower_part = " ("+bases[type]+ ")";
     return lower_part;
 }
 //
-void validate_input(int type){
+array<int,8> validate_input(int type){
     if(type<INPUT_SIZE_CHECK || type>INPUT_SIZE_CHECK*10){
-        throw std::bad_typeid();
+        throw std::out_of_range("size is out of range");
     }
-    for (int i = 0; i < 8; ++i) {
+    array<int,8> types;
+    for (int i = 7; i >= 0; --i) {
         if(type%10<1||type%10>4){
-            throw std::out_of_range();
+            throw std::out_of_range("value out of range");
         }
+        types[i] = type%10;
         type = type/10;
     }
+    return types;
 }
+
 std::string build_snowman(array<int,8> types){
-    std::string base = build_base(types[7]-1);
-    std::string body = build_body(types[6]-1);
-    std::string hat = " "+hats[types[0]-1];
+    std::string base = build_base(types[BASE]-1);
+    std::string body = build_body(types);
+    std::string hat = hats[types[HAT]-1];
     std::string face = build_face(types);
-    return hat+"\n"+left_arm_tops[types[4]-1]+face+right_arm_tops[types[5]-1]+"\n"+left_arms[types[4]-1]+body+right_arms[types[5]-1]
-    +"\n"
+    return hat+"\n"+face+"\n"+body+"\n"+base;
 }
 string ariel::snowman(int type){
-    validate_input(type);
-    if(type<INPUT_SIZE_CHECK || type>INPUT_SIZE_CHECK*10){
-        throw std::bad_typeid();
-    }
-//    if(type==snowman1){
-//        return "_===_\n"
-//               "(.,.)\n"
-//               "( : )\n"
-//               "( : )";
-//    }
-//    if(type== snowman2){
-//        return "  _\n"
-//               " /_\\\n"
-//               "\\(o_O)\n"
-//               " (] [)>\n"
-//               " (   )";
-//    }
-//    array<int,8> input = parse_input(type);
-    std::string snowman_print = hats[type/INPUT_SIZE_CHECK];
-    type = type%INPUT_SIZE_CHECK;
-    build_snowman(type);
-    return "asas";
+    array<int,8> types = validate_input(type);
+    return build_snowman(types);
 }
 //
-//int main(){
-////     string str;
-////     int typeNumber=0;
-////     std::cin >> str >> typeNumber;
-////     if(validate_input(str)!=0){
-////         ariel::snowman(typeNumber);
-////     }else{
-////         exit(0);
-////     }
-//     return 0;
-//}
+int main(){
+    std::cout<< ariel::snowman(11114411);
+    return 0;
+}
